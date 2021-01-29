8cc6babe4293dfb719dcebe26e01e2d0ac095ec0
PratinavGupta <28yashicagupta@gmail.com>
2021-01-29 19:43:08 +0530

    Add Slingshot mechanism

--- Chain.js
+++ Chain.js
@@ -1,20 +1,20 @@
-class Chain{
-    constructor(bodyA, bodyB){
-        var options = {
-            bodyA: bodyA,
-            bodyB: bodyB,
-            stiffness: 0.04,
-            length: 10
-        }
+class Chain {
+    constructor(a, b) {
+        var options = { bodyA: a, pointB: b, stiffness: 0.04, length: 10 }
         this.chain = Constraint.create(options);
         World.add(world, this.chain);
     }
 
-    display(){
-        var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
-        strokeWeight(4);
-        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    display() {
+        if (this.chain.bodyA) {
+            var pointA = this.chain.bodyA.position;
+            var pointB = this.chain.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
     }
-    
+
+    fly() {
+        this.chain.bodyA = null;
+    }
 }
